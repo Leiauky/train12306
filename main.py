02582e220c093a08726302b163682085050714d1
Eliauky <65895123+Leiauky@users.noreply.github.com>
2023-04-28 23:29:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -29,8 +29,11 @@ def buy_ticket(train_date: str, from_station: str, to_station: str, trains_code:
 
 
 if __name__ == '__main__':
+    # 用户名
     username = UserConfig.USERNAME
+    # 密码
     password = UserConfig.PASSWORD
+    # 身份证后四位
     cast_num = UserConfig.CAST_NUM
     user = User(username=username, password=password, cast_num=cast_num, headless=False)
     isLogin = user.password_slider_login()
@@ -38,10 +41,16 @@ if __name__ == '__main__':
     # isLogin = user.qr_login()
     if not isLogin:
         raise MyError('登录失败')
+    # 乘车日期
     train_date = '2023-04-25'
+    # 上车地点
     from_station = '南昌'
+    # 下车地点
     to_station = '上海'
+    # 车次
     trains_code = ['K353', 'K1558']
+    # 乘客
+    # passengers = {'名字': (座位类型, 车票类型), }
     passengers = {'张三': (SeatType.HARD_SEAT, TicketType.ADULT_TICKETS), }
     buy_ticket(train_date=train_date, from_station=from_station, to_station=to_station, trains_code=trains_code,
                passengers=passengers)
